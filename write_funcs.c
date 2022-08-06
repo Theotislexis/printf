a24fd66afccf6e88284095d380b8b8ab82c9690b
Rasheedat71 <yrasheedat12@gmail.com>
2022-08-06 03:41:10 -0700

    Create write_funcs.c

--- write_funcs.c
+++ write_funcs.c
@@ -9,22 +9,23 @@
  * Description: _putchar uses a local buffer of 1024 to call write
  * as little as possible
  */
+
 int _putchar(char c)
 {
-	static char buf[1024];
-	static int i;
+static char buf[1024];
+static int i;
 
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+if (c == -1 || i >= 1024)
+{
+write(1, &buf, i);
+i = 0;
+}
+if (c != -1)
+{
+buf[i] = c;
+i++;
+}
+return (1);
 }
 
 /**
@@ -32,11 +33,12 @@ int _putchar(char c)
  * @str: pointer to the string to print
  * Return: number of chars written
  */
-int _puts(char *str)
-{
-	register int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
+int _puts(char *str)
+
+{
+register int i;
+for (i = 0; str[i] != '\0'; i++)
+_putchar(str[i]);
+return (i);
 }
