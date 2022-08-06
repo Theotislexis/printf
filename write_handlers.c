cc97d2ada9cfbb829eeb508ace9415c85222237f
Rasheedat Olufunke Yusuf <106766855+Rasheedat71@users.noreply.github.com>
2022-08-06 21:06:38 +0100

    Create write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -12,6 +12,7 @@
  *
  * Return: Number of chars printed.
  */
+
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
 { /* char is stored at left and paddind at buffer's right */
