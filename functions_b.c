ae891d7d21f66dd28364c8476657f5d3adac3b44
Rasheedat Olufunke Yusuf <106766855+Rasheedat71@users.noreply.github.com>
2022-08-06 20:49:09 +0100

    Create functions_b.c

--- functions_b.c
+++ functions_b.c
@@ -11,6 +11,7 @@
  * @size: Size specifier
  * Return: Number of chars printed.
  */
+
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -63,6 +64,7 @@ int print_pointer(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
+
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -146,6 +148,7 @@ int print_reverse(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Numbers of chars printed
  */
+
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
