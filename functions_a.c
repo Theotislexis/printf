79488fd0e2f5d489f9c636c52209a1eb9e21d081
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-01-24 01:09:12 -0800

    functions_a.c

--- functions_a.c
+++ functions_a.c
@@ -2,6 +2,7 @@
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
+ * Update code
  * print_unsigned - Prints an unsigned number
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
